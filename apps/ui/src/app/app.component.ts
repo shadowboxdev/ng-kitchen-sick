a6d15d3b5b28d3a3d18a14df1d9095ab5804a9d7
Paul Fuller <pfuller@360.com>
2022-08-03 23:36:53 -0700

    chore(ui): clean up app component

--- apps/ui/src/app/app.component.ts
+++ apps/ui/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-
-import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
 
 import { KeycloakService } from 'keycloak-angular';
 
@@ -11,41 +8,23 @@ import { KeycloakService } from 'keycloak-angular';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  private readonly _mobileQueryListener!: () => void;
-
   public isLoggedIn: boolean = false;
-  public readonly mobileQuery!: MediaQueryList;
-  public readonly fillerNav = Array.from(
-    { length: 50 },
-    (_, i) => `Nav Item ${i + 1}`
-  );
 
-  constructor(
-    private readonly _cd: ChangeDetectorRef,
-    public readonly media: MediaMatcher,
-    private readonly keycloak: KeycloakService,
-    http: HttpClient
-  ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this._mobileQueryListener = () => this._cd.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
-
-    http.get('api/hello').subscribe(console.log);
-  }
+  constructor(private readonly _keycloak: KeycloakService) {}
 
   public async ngOnInit(): Promise<void> {
-    this.isLoggedIn = await this.keycloak.isLoggedIn();
+    this.isLoggedIn = await this._keycloak.isLoggedIn();
 
     if (this.isLoggedIn) {
-      const userProfile = await this.keycloak.loadUserProfile();
+      const userProfile = await this._keycloak.loadUserProfile();
     }
   }
 
   public login(): void {
-    this.keycloak.login();
+    this._keycloak.login();
   }
 
   public logout(): void {
-    this.keycloak.logout();
+    this._keycloak.logout();
   }
 }
